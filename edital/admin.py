426b3d36d319773f11c7e6782ccf1170f98180d7
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-02 11:09:53 -0400

    correção no modulo edital

--- edital/admin.py
+++ edital/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Edital
+from .models import Edital, PublicacaoEdital
 
 @admin.register(Edital)
 class EditalAluno(admin.ModelAdmin):
-    list_display = ('descricao', 'numero', 'ano', 'data_inicio', 'data_fim')  # 'IncEm', 'AltEm', 'IncPor', 'AltPor', 'status_aluno')
+    list_display = ('descricao', 'numero', 'ano', 'data_hora_inicio', 'data_hora_fim')  # 'IncEm', 'AltEm', 'IncPor', 'AltPor', 'status_aluno')
 
 
+
+
+@admin.register(PublicacaoEdital)
+class EtapaEdital(admin.ModelAdmin):
+    list_display = ('publicacao_descricao', 'publicacao_anexo_arq', 'publicacao_data')
--- edital/models.py
+++ edital/models.py
@@ -1,12 +1,14 @@
 from core.models import Base
 from django.db import models
 
+
 class Edital(Base):
     descricao = models.CharField('Descrição', max_length=100)
     numero = models.PositiveIntegerField('Nº do Edital')
     ano = models.PositiveIntegerField('Informe o ano do Edital')
-    data_inicio = models.DateTimeField('Validade Data/Hora Inicio:', null=True)
-    data_fim = models.DateTimeField('Validade Data/Hora Inicio:', null=True)
+    data_hora_inicio = models.DateTimeField('Validade Data/Hora Inicio:', null=True)
+    data_hora_fim = models.DateTimeField('Validade Data/Hora Inicio:', null=True)
+    edital_status = models.BooleanField('Edital ativo?', default=True)
 
     class Meta:
         verbose_name = 'Edital'
@@ -14,3 +16,20 @@ class Edital(Base):
 
     def __str__(self):
         return self.descricao
+
+
+
+
+class PublicacaoEdital(Base):
+    edital = models.ForeignKey("Edital", on_delete=models.CASCADE)
+    publicacao_descricao = models.CharField('Descrição', max_length=150)
+    publicacao_anexo_arq = models.CharField(max_length=100)
+    publicacao_data = models.DateField('Data da publicação', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.publicacao_descricao
+
+    class Meta:
+        verbose_name = 'Publicação'
+        verbose_name_plural = 'Publicações'
