76b42a2edead2c5e08f4780b13a78454df77381a
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-02 19:04:17 -0400

    uninstall admin-lte2 e 3

--- sisnae/settings.py
+++ sisnae/settings.py
@@ -32,8 +32,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django_adminlte',
-    'django_adminlte_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
