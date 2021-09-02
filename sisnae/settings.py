539bfa99b389bf1c2ec3c9efad2abc391995f153
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-02 18:41:50 -0400

    instalando django admin-lte2

--- sisnae/settings.py
+++ sisnae/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_adminlte',
+    'django_adminlte_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
