7d3d34b597fbcaea616ba708c3d9a795fb567c8b
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-07 13:11:56 -0400

    recuperando edital na index

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import TemplateView
+from edital.models import Edital
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['editais'] = Edital.objects.all()
+        return  context
 
 class PerguntaFreqView(TemplateView):
     template_name = 'perguntas_frequente.html'
