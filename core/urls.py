0f19b23f3d35c308fc30b2fa07626cea21fb64b4
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-07 12:21:58 -0400

    criando pergunta frequentes e separando os codigos html com extends

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, PerguntaFreqView
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('perguntas_frequente/', PerguntaFreqView.as_view(), name='perguntas_frequente'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class PerguntaFreqView(TemplateView):
+    template_name = 'perguntas_frequente.html'
