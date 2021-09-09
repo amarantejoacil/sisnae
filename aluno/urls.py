4342bdc0fb12168efd8e25698b7c87b43d4bfbd5
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-09 12:33:44 -0400

    se a rota for só /aluno redireciona para index

--- aluno/urls.py
+++ aluno/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from core.views import IndexView
 
 from .views import PrimeiroAcessoAlunoView
 urlpatterns = [
+    path('', IndexView.as_view(), name='primeiro_acesso_aluno'),
     path('primeiro_acesso_aluno', PrimeiroAcessoAlunoView.as_view(), name='primeiro_acesso_aluno'),
 ]
