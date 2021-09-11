7b98fdfeb931784e27ed00ab5ee9f73c3053dd0f
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-11 13:29:59 -0400

    criando tela de detalhes editais

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from core.views import IndexView
+
 
 from .views import PrimeiroAcessoAlunoView, PainelAlunoView
 from .views import UsuarioView, LogoutView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', PainelAlunoView.as_view(), name='painel_aluno'),
     path('usuario/', UsuarioView.as_view(), name='usuario'),
     path('primeiro_acesso_aluno/', PrimeiroAcessoAlunoView.as_view(), name='primeiro_acesso_aluno'),
     path('painel_aluno/', PainelAlunoView.as_view(), name='painel_aluno'),
