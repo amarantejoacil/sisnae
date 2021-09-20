28a8ccaafe5a8960a3b3401deb2a7f1829bcb6bb
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-20 15:43:13 -0400

    recuperando nome do aluno

--- aluno/admin.py
+++ aluno/admin.py
@@ -3,14 +3,22 @@ from .models import Aluno
 
 @admin.register(Aluno)
 class AdminAluno(admin.ModelAdmin):
-    list_display = ('id', 'aluno_matricula', 'aluno_status')#, 'IncEm', 'AltEm', 'IncPor', 'AltPor')
-    list_display_links = ('aluno_matricula', )
+    list_display = ('get_nome', 'aluno_matricula', 'aluno_status', 'curso',)#, 'IncEm', 'AltEm', 'IncPor', 'AltPor')
+    list_display_links = ('get_nome', 'aluno_matricula')
     search_fields = ('aluno_matricula',)
-    list_filter = ('aluno_matricula', 'aluno_status')
+    list_filter = ('aluno_matricula',)
     #list_editable = ('pessoa_status',)
     list_per_page = 15
 
     readonly_fields = ('IncPor', 'AltPor', 'IncEm', 'AltEm')
+
+    def get_nome(self, obj):
+
+        return obj.pessoa.nome
+    get_nome.short_description = 'Nome'
+
+
+
 
 
 
