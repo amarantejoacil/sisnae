7cdb2a40605495606f7c3b3ee03319f0ce87accc
Joacil Amarante <joacil.amarante@gmail.com>
2021-09-29 18:56:11 -0400

    ajuste table todos_editais.html

--- core/views.py
+++ core/views.py
@@ -33,20 +33,39 @@ class DetalheEditaisView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['detalhe_editais'] = Edital.objects.all()
+        # print('context-----------------------')
+        # print(context)
+        return context
+
+class DetalhePublicacoesEditaisView(ListView):
+    #modelo 3
+    model = PublicacaoEdital
+    template_name = 'detalhe_editais.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['detalhe_publicacoes'] = PublicacaoEdital.objects.all()
         print('context-----------------------')
         print(context)
         return context
 
-class DetalhePublicacoesEditaisView(ListView):
+
+
+
+
+
+
+
+
     # modelo 2 listview
-    model = PublicacaoEdital
-    paginate_by = 20
-    template_name = 'detalhe_editais.html'
-    queryset = PublicacaoEdital.objects.all()
-    print('queryset-----------------------')
-    print(queryset)
-    context_object_name = 'publicacao_editais'
-    print(context_object_name)
+    # model = PublicacaoEdital
+    # paginate_by = 20
+    # template_name = 'detalhe_editais.html'
+    # queryset = PublicacaoEdital.objects.all()
+    # print('queryset-----------------------')
+    # print(queryset)
+    # context_object_name = 'publicacao_editais'
+    # print(context_object_name)
 
 
     #modelo 1
