209c400c96e4a7cfe9b2f768998cbbe0517d0c0a
Joacil Amarante <joacil.amarante@gmail.com>
2021-10-08 10:26:29 -0400

    ajuste form-upload.html

--- importacao/urls.py
+++ importacao/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import ImportacaoView
+from .views import ImportacaoView, ListDadosImportacaoView, CreateDadosImportacaoView
 
 urlpatterns = [
     path('', ImportacaoView.as_view(), name='ImportacaoView'),
+    path('listar-importacao', ListDadosImportacaoView.as_view(), name='ListDadosImportacaoView'),
+    path('cadastro-importacao', CreateDadosImportacaoView.as_view(), name='CreateDadosImportacaoView'),
 ]
--- importacao/views.py
+++ importacao/views.py
@@ -1,7 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Importacao
+from django.urls import reverse_lazy
 
 
 class ImportacaoView(TemplateView):
     template_name = 'importacao.html'
 
+
+class CreateDadosImportacaoView(ListView):
+    login_url = reverse_lazy('index')
+    model = Importacao
+    fields = ['id', 'descricao', 'arquivo', ]
+    template_name = 'form-upload.html'
+    success_url = reverse_lazy('ListDadosImportacaoView')
+
+
+class ListDadosImportacaoView(ListView):
+    model = Importacao
+    template_name = 'list-importacao.html'
+    paginate_by = 10
+    ordering = '-id'
 
